87d6ae9203f245fd47e87487e3e3bc14ca9e9c6e
Neftali Yagua <neftali.yagua@cortex-ia.com.co>
2025-12-15 14:59:27 -0500

    fix: corregir rutas de relay-ejemplo-cine a cine

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 			strict: false
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/relay-ejemplo-cine' : ''
+			base: process.env.NODE_ENV === 'production' ? '/cine' : ''
 		}
 	}
 };
